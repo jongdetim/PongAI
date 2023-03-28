fa3bd860e1821a5ff364722861c6ad3930a40bf6
Tim de Jong <timdedenker@gmail.com>
2023-03-28 19:58:21 +0200

    added neat_pong.py, which has some boilerplate neat-python code

---
+++ neat_pong.py
@@ -0,0 +1,44 @@
+import pygame
+import neat
+
+# Define the fitness function for evaluating the fitness of each genome
+def evaluate_genome(genome, config):
+    # Create a new neural network for this genome
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+
+    # Initialize the game and play a game using the neural network
+    game = Game()
+    while not game.done:
+        # Get the current state of the game
+        state = game.get_state()
+
+        # Feed the state through the neural network to get the action
+        action = net.activate(state)
+
+        # Take the action in the game
+        game.take_action(action)
+
+    # Return the fitness of the genome (in this case, the score of the game)
+    return game.score
+
+# Load the NEAT configuration file
+config_path = "path/to/neat-config-file.txt"
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+# Create a new population of genomes using the configuration
+population = neat.Population(config)
+
+# Add a reporter to output the progress of the algorithm during training
+stats = neat.StatisticsReporter()
+population.add_reporter(stats)
+
+# Run the NEAT algorithm for up to 100 generations
+winner = population.run(evaluate_genome, 100)
+
+# Use the best genome found during training to play a game of Pong
+net = neat.nn.FeedForwardNetwork.create(winner, config)
+game = Game()
+while not game.done:
+    state = game.get_state()
+    action = net.activate(state)
+    game.take_action(action)
