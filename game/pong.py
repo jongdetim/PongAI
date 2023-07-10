c367cbf793f7b4f62c678276fa282057c1483b75
tim <timdedenker@gmail.com>
2023-07-10 04:13:57 +0200

    added multiprocessing, tweaked config.ini params. can't seem to get a good model trained

--- game/pong.py
+++ game/pong.py
@@ -1,15 +1,17 @@
 import math
+import random
 import pygame
 
-# define colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+# define global colors
+# BLACK = (0, 0, 0)
+# WHITE = (255, 255, 255)
 
-BALL_SPEED_INCREASE = 25
-PADDLE_SPEED = 350
+# define global game parameters
+# BALL_SPEED_INCREASE = 25
+# PADDLE_SPEED = 350
 
 class Ball:
-    def __init__(self, x, y, width, height, color, vel, window_width, window_height):
+    def __init__(self, x, y, width, height, color, vel, window_width, window_height, speed_increase):
         self.x = x
         self.prev_x = x
         self.y = y
@@ -20,6 +22,7 @@ class Ball:
         self.vel = vel
         self.window_width = window_width
         self.window_height = window_height
+        self.speed_increase = speed_increase
 
     def move(self, dt):
         self.prev_x = self.x
@@ -37,28 +40,71 @@ class Ball:
             round(self.x), round(self.y), self.width, self.height))
 
 class Paddle:
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, color, left):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
+        self.left = left
         self.vel = 0
+        self.hits = 0
 
     def move(self, window_height, dt):
         self.y = max(min(self.y + self.vel / (1000 / dt),
                      window_height - self.height), 0)
+
+    def bounce_ball_if_collision(self, ball):
+        if self.left:
+            if ball.x <= self.x + self.width and ball.prev_x > self.x + self.width:
+                t, intersect_y = calculate_interpolation(
+                    ball.prev_x, ball.x, ball.prev_y, ball.y, self.x + self.width)
+                if self.check_collision_y(ball, intersect_y):
+                    bounce_angle = self.calculate_bounce_angle(intersect_y)
+                    self.adjust_ball_velocity(ball, bounce_angle)
+                    ball.x = self.x + ball.width
+                    ball.y = intersect_y
+                    self.hits += 1
+        else:
+            if ball.x + ball.width >= self.x and ball.prev_x + ball.width < self.x:
+                t, intersect_y = calculate_interpolation(
+                    ball.prev_x + ball.width, ball.x + ball.width, ball.prev_y, ball.y, self.x)
+                if self.check_collision_y(ball, intersect_y):
+                    bounce_angle = self.calculate_bounce_angle(
+                        intersect_y)
+                    self.adjust_ball_velocity(ball, bounce_angle)
+                    ball.x = self.x - ball.width
+                    ball.y = intersect_y
+                    self.hits += 1
+
+    def check_collision_y(self, ball, intersect_y):
+        return self.y - ball.height <= intersect_y <= self.y + self.height + ball.height
+
+    def calculate_bounce_angle(self, intersect_y):
+        relative_intersect_y = (self.y + self.height / 2) - intersect_y
+        normalized_intersect_y = relative_intersect_y / (self.height / 2)
+        bounce_angle = normalized_intersect_y * math.pi / 3
+        return bounce_angle
+
+    def adjust_ball_velocity(self, ball, bounce_angle):
+        ball.vel[0] = abs(ball.vel[0]) + ball.speed_increase if self.left else -abs(ball.vel[0]) - ball.speed_increase
+        ball.vel[1] = -ball.vel[0] * math.sin(bounce_angle) if self.left else ball.vel[0] * math.sin(bounce_angle)
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, pygame.Rect(
             round(self.x), round(self.y), self.width, self.height))
         
 class GodPaddle(Paddle):
-    def __init__(self, x, y, width, height, color):
-        super().__init__(x, y, width, height, color)
+    def __init__(self, x, y, width, height, color, left):
+        super().__init__(x, y, width, height, color, left)
         
     def move(self, *args):
         pass
+
+    def calculate_bounce_angle(self, intersect_y):
+        # Calculate a random angle for the ball's bounce
+        bounce_angle = random.uniform(-math.pi/3, math.pi/3)
+        return bounce_angle
 
     def draw(self, *args):
         super().draw(*args)
@@ -66,32 +112,33 @@ class GodPaddle(Paddle):
 
 class Game:
 
-    # BLACK = (0, 0, 0)
-    # WHITE = (255, 255, 255)
-    # BALL_SPEED_INCREASE = 25
-    # PADDLE_SPEED = 350
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    BALL_SPEED_INCREASE = 25
+    PADDLE_SPEED = 350
+    MAX_SCORE = 3
 
-    def __init__(self, window_width=800, window_height=600, render=True, player1: Paddle=None, player2: Paddle=None):
+    def __init__(self, window_width=800, window_height=600, render=True, player1: Paddle=None, player2: Paddle=None, vsync=True):
         pygame.display.set_caption("Pong")
         self.window_width = window_width
         self.window_height = window_height
         self.screen = pygame.display.set_mode(
-            (self.window_width, self.window_height), flags=pygame.SCALED, vsync=1) if render else None
+            (self.window_width, self.window_height), flags=pygame.SCALED, vsync=vsync) if render else None
         self.render_clock = pygame.time.Clock()
         self.logic_clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, 36)
         # initialize paddles and ball
         if player1 is None:
-            self.paddle1 = Paddle(50, 250, 10, 100, WHITE)
+            self.paddle1 = Paddle(50, 250, 10, 100, self.WHITE, True)
         else:
             self.paddle1 = player1
         if player2 is None:
-            self.paddle2 = Paddle(750 - 10, 250, 10, 100, WHITE)
+            self.paddle2 = Paddle(750 - 10, 250, 10, 100, self.WHITE, False)
         else:
             self.paddle2 = player2
 
-        self.ball = Ball(400, 300, 10, 10, WHITE, [
-                         175, 175], window_width, window_height)
+        self.ball = Ball(400, 300, 10, 10, self.WHITE, [
+                         175, 175], window_width, window_height, self.BALL_SPEED_INCREASE)
         self.score1 = 0
         self.score2 = 0
         self.done = False
@@ -104,7 +151,7 @@ class Game:
 
         # Draw scores
         score_text = f"{self.score1} - {self.score2}"
-        text_surface = self.font.render(score_text, True, WHITE)
+        text_surface = self.font.render(score_text, True, self.WHITE)
         text_rect = text_surface.get_rect(center=(self.window_width/2, 50))
         self.screen.blit(text_surface, text_rect)
 
@@ -121,11 +168,13 @@ class Game:
             return False
         # input is 1 for up, 2 for down, 0 for no input
         if bot_input is not None:
-            self.paddle1.vel = -PADDLE_SPEED if input == 1 else PADDLE_SPEED if input == 2 else 0
+            self.paddle1.vel = -self.PADDLE_SPEED if bot_input == 1 else self.PADDLE_SPEED if bot_input == 2 else 0
         self.update_game_logic(dt)  # passes a constant dt value
+        if self.check_game_over():
+            return False
 
         if render:
-            self.screen.fill(BLACK)
+            self.screen.fill(self.BLACK)
             self.draw_objects()
             # pygame.display.update()
             pygame.display.flip()
@@ -135,7 +184,7 @@ class Game:
     def run(self, tickrate=60, render_fps=60):
         '''Runs the game loop with decoupled tickrate and fps until the user quits the game'''
         last_render_time = pygame.time.get_ticks()
-        while True:
+        while not self.done:
             # Update game state
             # print("updating game logic")
             dt = self.logic_clock.tick(tickrate)
@@ -143,13 +192,14 @@ class Game:
             if not self.handle_events():
                 break
             self.update_game_logic(dt)
+            self.check_game_over()
 
             # Render the frame
             if pygame.time.get_ticks() - last_render_time >= 1000 / render_fps:
                 # print("rendering frame")
                 # print("frametime: ", pygame.time.get_ticks() - last_render_time, "ms")
                 last_render_time = pygame.time.get_ticks()
-                self.screen.fill(BLACK)
+                self.screen.fill(self.BLACK)
                 self.draw_objects()
                 pygame.display.update()
 
@@ -166,16 +216,16 @@ class Game:
 
         keys = pygame.key.get_pressed()
         # Changes paddle velocity if key is pressed
-        self.paddle2.vel = -PADDLE_SPEED if keys[pygame.K_UP] else PADDLE_SPEED if keys[pygame.K_DOWN] else 0
-        self.paddle1.vel = -PADDLE_SPEED if keys[pygame.K_w] else PADDLE_SPEED if keys[pygame.K_s] else 0
+        self.paddle2.vel = -self.PADDLE_SPEED if keys[pygame.K_UP] else self.PADDLE_SPEED if keys[pygame.K_DOWN] else 0
+        self.paddle1.vel = -self.PADDLE_SPEED if keys[pygame.K_w] else self.PADDLE_SPEED if keys[pygame.K_s] else 0
 
         return True
 
     def update_game_logic(self, dt):
         self.move_paddles(dt)
         self.move_ball(dt)
-        self.check_collision_with_paddle1()
-        self.check_collision_with_paddle2()
+        self.paddle1.bounce_ball_if_collision(self.ball)
+        self.paddle2.bounce_ball_if_collision(self.ball)
         self.check_collision_with_walls()
         self.check_ball_off_screen()
 
@@ -185,51 +235,7 @@ class Game:
 
     def move_ball(self, dt):
         self.ball.move(dt)
-
-    def check_collision_with_paddle1(self):
-        if self.ball.x <= self.paddle1.x + self.ball.width and self.ball.prev_x > self.paddle1.x + self.ball.width:
-            t, intersect_y = self.calculate_interpolation(
-                self.ball.prev_x, self.ball.x, self.ball.prev_y, self.ball.y, self.paddle1.x + self.paddle1.width)
-            if self.check_collision_y(intersect_y, self.paddle1):
-                bounce_angle = self.calculate_bounce_angle(
-                    intersect_y, self.paddle1)
-                self.adjust_ball_velocity(self.paddle1, bounce_angle)
-                self.ball.x = self.paddle1.x + self.paddle1.width
-                self.ball.y = intersect_y
-
-    def check_collision_with_paddle2(self):
-        if self.ball.x + self.ball.width >= self.paddle2.x and self.ball.prev_x + self.ball.width < self.paddle2.x:
-            t, intersect_y = self.calculate_interpolation(
-                self.ball.prev_x + self.ball.width, self.ball.x + self.ball.width, self.ball.prev_y, self.ball.y, self.paddle2.x)
-            if self.check_collision_y(intersect_y, self.paddle2):
-                bounce_angle = self.calculate_bounce_angle(
-                    intersect_y, self.paddle2)
-                self.adjust_ball_velocity(self.paddle2, bounce_angle)
-                self.ball.x = self.paddle2.x - self.ball.width
-                self.ball.y = intersect_y
-
-    def calculate_interpolation(self, x1, x2, y1, y2, target_x):
-        t = (target_x - x1) / (x2 - x1)
-        intersect_y = y1 + (y2 - y1) * t
-        return t, intersect_y
-
-    def check_collision_y(self, intersect_y, paddle):
-        return paddle.y - self.ball.height <= intersect_y <= paddle.y + paddle.height + self.ball.height
-
-    def calculate_bounce_angle(self, intersect_y, paddle):
-        relative_intersect_y = (paddle.y + paddle.height / 2) - intersect_y
-        normalized_intersect_y = relative_intersect_y / (paddle.height / 2)
-        bounce_angle = normalized_intersect_y * math.pi / 3
-        return bounce_angle
-
-    def adjust_ball_velocity(self, paddle, bounce_angle):
-        if paddle is self.paddle1:
-            self.ball.vel[0] = abs(self.ball.vel[0]) + BALL_SPEED_INCREASE
-            self.ball.vel[1] = -self.ball.vel[0] * math.sin(bounce_angle)
-        else:
-            self.ball.vel[0] = -abs(self.ball.vel[0]) - BALL_SPEED_INCREASE
-            self.ball.vel[1] = self.ball.vel[0] * math.sin(bounce_angle)
-
+    
     def check_collision_with_walls(self):
         if self.ball.y <= 0:
             self.ball.vel[1] *= -1
@@ -245,16 +251,32 @@ class Game:
         elif self.ball.x > self.window_width:
             self.score1 += 1
             self.ball.reset_position(-1)
+        
+    def check_game_over(self):
+        if self.score1 >= self.MAX_SCORE or self.score2 >= self.MAX_SCORE:
+            self.done = True
+            return True
+        return False
 
     def get_game_state(self):
         return [self.ball.x, self.ball.y, self.ball.vel[0], self.ball.vel[1], \
                     self.paddle1.y, self.paddle2.y]
+        
+    def get_scaled_game_state(self):
+        return [self.ball.x / 800, self.ball.y / 600, self.ball.vel[0] / 300, self.ball.vel[1] / 300, \
+                    self.paddle1.y / 600, self.paddle2.y / 600]
+
+
+def calculate_interpolation(x1, x2, y1, y2, target_x):
+    t = (target_x - x1) / (x2 - x1)
+    intersect_y = y1 + (y2 - y1) * t
+    return t, intersect_y
 
 
 if __name__ == '__main__':
     pygame.init()
     window_size = (800, 600)
-    game = Game(*window_size, render=True)
-    while game.run_one_frame(input=None, dt=1000/60, render=True, tickrate=60):
+    game = Game(*window_size, render=True, player2=GodPaddle(750 - 10, 0, 10, 600, Game.WHITE, False))
+    while game.run_one_frame(bot_input=None, dt=1000/60, render=True, tickrate=60):
         # print(game.get_game_data())
         pass
--- neat/train_neat.py
+++ neat/train_neat.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import multiprocessing
 
 here = os.path.dirname(__file__)
 sys.path.append(os.path.join(here, '..'))
@@ -13,63 +14,121 @@ from game.pong import *
 
 window_size = (800, 600)
 
-leaky_relu = lambda x: max(0.01*x, x)
+def leaky_relu(x):
+    return max(0.01*x, x)
+
+def custom_init(genome_config, _):
+    # Disable connections from the last input node
+    num_inputs = genome_config.num_inputs
+    num_hidden = genome_config.num_hidden
+    num_outputs = genome_config.num_outputs
+
+    last_input_node = num_inputs - 1
+
+    for j in range(num_hidden, num_hidden + num_outputs):
+        genome_config.init_connectivity[(last_input_node, j)] = 0.0
 
 # Define the fitness function for evaluating the fitness of each genome
 def evaluate_genomes(genomes, config):
     for genome_id, genome in genomes:
-        print("genome:", genome)
+        # print("genome:", genome)
+        # quit()
         
         # Create a new neural network for this genome
         net = neat.nn.FeedForwardNetwork.create(genome, config)
 
         # Initialize the game and play a game using the neural network
-        game = Game(*window_size, render=True, player2=GodPaddle())
+        game = Game(*window_size, render=True, player2=GodPaddle(750 - 10, 0, 10, 600, Game.WHITE, False), vsync=False)
         while not game.done:
             # Get the current state of the game
             state = game.get_game_state()
-            print("game state:", state)
+            # print("game state:", state)
 
             # Feed the state through the neural network to get the action
-            action = net.activate(state)
-            print("genome output:", action)
+            action = net.activate(state[0:5])
+            # print("genome output:", action)
+            
+            # take highest value output neuron index
             action = np.argmax(action)
 
             # Take the action in the game
-            game.run_one_frame(bot_input=action, dt=1000/60, render=True, tickrate=60)
+            game.run_one_frame(bot_input=action, dt=1000/60, render=False, tickrate=1000000000)
 
-        # set the fitness of the genome (in this case, the score of the game)
-        genome.fitness = game.score1
-        # only works for left paddle!!
+        # set the fitness of the genome
+        genome.fitness = game.paddle1.hits
+        print(game.paddle1.hits)
+        # only works for left paddle!! and counts the amount of hits, not the actual game score.
 
-# Load the NEAT configuration file
-config_path = "./neat/config.ini"
-config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
-config.genome_config.add_activation('leakyrelu', leaky_relu)
+# Define the fitness function for evaluating the fitness of each genome
+def evaluate_genome(genome, config):        
+    # Create a new neural network for this genome
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+    
+    pygame.font.init()
 
+    # Initialize the game and play a game using the neural network
+    game = Game(*window_size, render=True, player2=GodPaddle(750 - 10, 0, 10, 600, Game.WHITE, False), vsync=False)
+    while not game.done:
+        # Get the current state of the game
+        state = game.get_game_state()
+        # print("game state:", state)
 
-# Create a new population of genomes using the configuration
-population = neat.Population(config)
+        # Feed the state through the neural network to get the action
+        action = net.activate(state[0:5])
+        # print("genome output:", action)
+        
+        # take highest value output neuron index
+        action = np.argmax(action)
 
-# Add a reporter to output the progress of the algorithm during training
-stats = neat.StatisticsReporter()
-population.add_reporter(stats)
-population.add_reporter(neat.StdOutReporter(True))
+        # Take the action in the game
+        game.run_one_frame(bot_input=action, dt=1000/60, render=False, tickrate=1000000000000)
 
-# initialize pygame
-pygame.init()
+    # set the fitness of the genome
+    print(game.paddle1.hits)
+    return game.paddle1.hits
+    # only works for left paddle!! and counts the amount of hits, not the actual game score.
 
-# Run the NEAT algorithm for up to 100 generations
-winner = population.run(evaluate_genomes, 100)
+def run():
+    # Load the NEAT configuration file
+    config_path = "./neat/config.ini"
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    config.genome_config.add_activation('leakyrelu', leaky_relu)
 
-# Use the best genome found during training to play a game of Pong
-net = neat.nn.FeedForwardNetwork.create(winner, config)
-pygame.init()
+    # Set the custom initialization function. this sets all connections to the last input node to 0. this is the paddle2 position
+    # config.genome_config.init_type = custom_init
 
-render = True
-game = Game(*window_size, render=render)
-running = True
-while running:
-    state = game.get_game_state()
-    action = net.activate(state)
-    running = game.run_one_frame(input=action, dt=1000/60, render=render, tickrate=60)
+    # Create a new population of genomes using the configuration
+    population = neat.Population(config)
+
+    # Add a reporter to output the progress of the algorithm during training
+    stats = neat.StatisticsReporter()
+    population.add_reporter(stats)
+    population.add_reporter(neat.StdOutReporter(True))
+
+    # initialize pygame
+    pygame.init()
+
+    # Run the NEAT algorithm for up to x generations
+    # winner = population.run(evaluate_genomes, 20)
+
+    # Run the NEAT algorithm on multiple cores for up to x generations
+    pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), evaluate_genome)
+    winner = population.run(pe.evaluate, 40)
+
+    print("winner fitness:", winner.fitness)
+    print(winner)
+
+    # Use the best genome found during training to play a game of Pong
+    net = neat.nn.FeedForwardNetwork.create(winner, config)
+
+    render = True
+    game = Game(*window_size, render=render)
+
+    while not game.done:
+        state = game.get_scaled_game_state()
+        action = net.activate(state[0:5])
+        action = np.argmax(action)
+        game.run_one_frame(bot_input=action, dt=1000/60, render=render, tickrate=60)
+
+if __name__ == "__main__":
+    run()
